91919b765909facd040b895273c168b9db58e4cc
Tyler <nuvallo.inc@gmail.com>
2019-10-08 11:59:02 -0400

    this keyword section

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
  * in your own words. explain the four principle for the "this" keyword below.
  *
- * 1. Global Binding
- * 2. Implicit Binding
+ * 1. Global Binding - Directs to the window object (viewport)
+ * 2. Implicit Binding -
  * 3. New Binding
- * 4.
+ * 4. Explicit Binding
  *
  * write out a code example of each explanation above
  */
@@ -12,10 +12,21 @@
 // Principle 1
 
 // code example for Window Binding
+console.log(this);
 
 // Principle 2
 
 // code example for Implicit Binding
+const Implicit = {
+  greeting: "Hello",
+  speak: function() {
+    // let name = "Tyler";
+    let name = prompt("Enter name here");
+    return `${this.greeting}, ${name.toUpperCase()}`;
+  }
+};
+
+console.log(Implicit.speak());
 
 // Principle 3
 
